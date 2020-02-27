cab0f629b76819c5b2bb603f3322cfccedc8bdfd
Ayush Saraf <ayush.saraf47@gmail.com>
2020-02-28 01:53:50 +0530

    Added : functionality to display pending interviews

--- src/com/main/interview_allocation/service/InterviewScheduler.java
+++ src/com/main/interview_allocation/service/InterviewScheduler.java
@@ -10,6 +10,7 @@ public class InterviewScheduler {
 
     private static int INTERVIEW_DURATION = 2;
     private static LocalTime INTERVIEW_START_TIME = LocalTime.of(9, 0);
+    private static LocalTime DAY_END_TIME = LocalTime.of(17, 0);
     private static LocalTime INTERVIEW_END_TIME = INTERVIEW_START_TIME.plusHours(INTERVIEW_DURATION);
 
     public List<Interview> scheduleInterviews(List<Attendee> attendeeList, List<Interviewer> interviewerList, List<InterviewRoom> roomList) {
@@ -17,6 +18,10 @@ public class InterviewScheduler {
         int roomNo = 0;
         List<Interview> interviews = new ArrayList<>();
         for (Attendee attendee : attendeeList) {
+            if (INTERVIEW_START_TIME.equals(DAY_END_TIME)) {
+                System.out.println("pending interviews of id : " + attendee.toString());
+                continue;
+            }
             Interviewer interviewer = interviewerList.get(interviewerNo++);
             InterviewRoom room = roomList.get(roomNo++);
             InterviewTime time = new InterviewTime(INTERVIEW_START_TIME, INTERVIEW_END_TIME);
