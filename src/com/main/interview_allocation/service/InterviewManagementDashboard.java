a7d46d2d5cd0c9afdbe02992ac830ce817545bdc
Ayush Saraf <ayush.saraf47@gmail.com>
2020-02-27 18:56:09 +0530

    Added : scheduling functionality to dashboard

--- src/com/main/interview_allocation/service/InterviewManagementDashboard.java
+++ src/com/main/interview_allocation/service/InterviewManagementDashboard.java
@@ -1,22 +1,29 @@
 package com.main.interview_allocation.service;
 
 import com.main.interview_allocation.model.Attendee;
-import com.main.interview_allocation.model.InterViewRoom;
+import com.main.interview_allocation.model.InterviewRoom;
+import com.main.interview_allocation.model.Interview;
 import com.main.interview_allocation.model.Interviewer;
 
 import java.util.List;
+import java.util.Map;
 
 public class InterviewManagementDashboard {
 
     private InterviewRegistrationService registrationService;
+    private InterviewScheduler scheduler;
 
     public InterviewManagementDashboard() {
         registrationService = new InterviewRegistrationService();
+        scheduler = new InterviewScheduler();
     }
 
     public void display() {
         List<Attendee> attendeesList = registrationService.registerAttendees();
+        Map<Attendee, Interview> interviews = registrationService.registerInterviewTimings(attendeesList);
         List<Interviewer> interviewersList = registrationService.registerInterviewers();
-        List<InterViewRoom> roomsList = registrationService.registerRooms();
+        List<InterviewRoom> roomsList = registrationService.registerRooms();
+        Map<Attendee, Interview> interviewMap = scheduler.scheduleInterviews(attendeesList, interviewersList, roomsList, interviews);
+        System.out.println(interviewMap.toString());
     }
 }
