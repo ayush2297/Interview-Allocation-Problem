93292ecc532140b905715aadcea5a01c6fd4cb77
Ayush Saraf <ayush.saraf47@gmail.com>
2020-02-28 11:58:07 +0530

    Refactored : made scheduler properties non static and local variables instead of global constants

--- src/com/main/interview_allocation/service/InterviewScheduler.java
+++ src/com/main/interview_allocation/service/InterviewScheduler.java
@@ -8,32 +8,33 @@ import java.util.List;
 
 public class InterviewScheduler {
 
-    private static int INTERVIEW_DURATION = 2;
-    private static LocalTime INTERVIEW_START_TIME = LocalTime.of(9, 0);
-    private static LocalTime DAY_END_TIME = LocalTime.of(17, 0);
-    private static LocalTime INTERVIEW_END_TIME = INTERVIEW_START_TIME.plusHours(INTERVIEW_DURATION);
-
     public List<Interview> scheduleInterviews(List<Attendee> attendeeList, List<Interviewer> interviewerList, List<InterviewRoom> roomList) {
+        int interviewDuration = 2;
+        LocalTime interviewStartTime = LocalTime.of(9, 0);
+        LocalTime interviewEndTime = interviewStartTime.plusHours(interviewDuration);
+        LocalTime dayEndTime = LocalTime.of(17, 0);
+
         int interviewerNo = 0;
         int roomNo = 0;
         List<Interview> interviews = new ArrayList<>();
+
         boolean isRoomCountMoreThanInterviewers = roomList.size() > interviewerList.size();
         for (Attendee attendee : attendeeList) {
-            if (INTERVIEW_START_TIME.equals(DAY_END_TIME)) {
+            if (interviewStartTime.equals(dayEndTime)) {
                 System.out.println("pending interviews of id : " + attendee.toString());
                 continue;
             }
             Interviewer interviewer = interviewerList.get(interviewerNo++);
             InterviewRoom room = roomList.get(roomNo++);
-            InterviewTime time = new InterviewTime(INTERVIEW_START_TIME, INTERVIEW_END_TIME);
+            InterviewTime time = new InterviewTime(interviewStartTime, interviewEndTime);
             interviewerNo = resetCounterIfAllUsed(interviewerNo, interviewerList.size());
             roomNo = resetCounterIfAllUsed(roomNo, roomList.size());
             if (!isRoomCountMoreThanInterviewers) {
                 if (roomNo == 0)
-                    updateInterviewTime();
+                    updateInterviewTime(interviewDuration, interviewStartTime, interviewEndTime);
             } else {
                 if (interviewerNo == 0)
-                    updateInterviewTime();
+                    updateInterviewTime(interviewDuration, interviewStartTime, interviewEndTime);
             }
             interviews.add(new Interview(attendee, interviewer, room, time));
         }
@@ -47,13 +48,14 @@ public class InterviewScheduler {
         return counter;
     }
 
-    private void updateInterviewTime() {
+    private InterviewTime updateInterviewTime(int interviewDuration, LocalTime interviewStartTime, LocalTime interviewEndTime) {
         int timeMultiplierFactor = 1;
-        LocalTime newTime = INTERVIEW_END_TIME.plusHours(INTERVIEW_DURATION);
-        if (newTime.isAfter(LocalTime.of(14, 1)) && newTime.isBefore(LocalTime.of(15,1))) {
+        LocalTime newTime = interviewEndTime.plusHours(interviewDuration);
+        if (newTime.isAfter(LocalTime.of(14, 1)) && newTime.isBefore(LocalTime.of(15, 1))) {
             timeMultiplierFactor++;
         }
-        INTERVIEW_START_TIME = INTERVIEW_START_TIME.plusHours(INTERVIEW_DURATION * timeMultiplierFactor);
-        INTERVIEW_END_TIME = INTERVIEW_END_TIME.plusHours(INTERVIEW_DURATION * timeMultiplierFactor);
+        interviewStartTime = interviewStartTime.plusHours(interviewDuration * timeMultiplierFactor);
+        interviewEndTime = interviewEndTime.plusHours(interviewDuration * timeMultiplierFactor);
+        return new InterviewTime(interviewStartTime, interviewEndTime);
     }
 }
